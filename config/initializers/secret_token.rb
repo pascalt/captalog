ffb096a1f6be22e54be445583f0958baaf8effd5
Pascal Toutain <pascaltoutain@gmail.com>
2010-11-11 15:26:33 +0100

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Captalog::Application.config.secret_token = '1d20ea8d25aee87b18b4ddac9a6a33a27ff418cb40d653c31bb67b257a38b8781db76d17380d0dbc29e0ac6f0a71f74ecfa550f7706ad5939c4e8037184354ab'
